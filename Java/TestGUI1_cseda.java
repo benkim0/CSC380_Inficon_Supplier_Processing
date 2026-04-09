1359d382eb4667a1b1e475250e3b5966938402c6
cseda <cseda@oswego.edu>
2026-04-08 22:44:07 -0400

    Messing around with GUI

--- Java/TestGUI1_cseda.java
+++ Java/TestGUI1_cseda.java
@@ -7,24 +7,21 @@ import java.lang.Process;
 public class TestGUI1_cseda {
 
         public static void main( String[] args) throws IOException {
-            ProcessBuilder pBuilder = new ProcessBuilder("python","C:\\Users\\super\\ForFunsies\\CSC380_Inficon_Supplier_Processing\\Python\\main.py","Vendor-Form.pdf");
+            ProcessBuilder pBuilder = new ProcessBuilder("\"C:\\Users\\super\\AppData\\Local\\Programs\\Python\\Python314\\python.exe\"","C:\\Users\\super\\ForFunsies\\CSC380_Inficon_Supplier_Processing\\Python\\main.py","C:\\Users\\super\\ForFunsies\\CSC380_Inficon_Supplier_Processing\\Python\\Vendor-Form.pdf");
             pBuilder.redirectErrorStream(true);
+            pBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
 
             JFrame frame = new JFrame("Test");
-            JButton uploadButton = new JButton("Upload");
+            JButton uploadButton = new JButton("Execute");
 
-            Icon icon = new ImageIcon("C:\\Users\\super\\Downloads\\gifs&memes\\skeleton-dance.gif\"");
 
-            uploadButton.setIcon(icon);
-
-            uploadButton.setBounds(30,30,80,30);
+            uploadButton.setBounds(30,30,200,30);
 
             uploadButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    //System.exit(0);
                     try {
-                        pBuilder.command();
+
                         Process fill = pBuilder.start();
 
                     } catch (IOException ex) {
@@ -36,9 +33,7 @@ public class TestGUI1_cseda {
 
             frame.add(uploadButton);
 
-
-
-            frame.setSize(1200,720);
+            frame.setSize(300,200);
             frame.setLayout(null);
             frame.setVisible(true);
         }
